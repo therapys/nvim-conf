b1f2d4f6c8a2a18987b4e4ee01f60938910efee1
therapys <karl.kiilaspaa@gmail.com>
2026-02-26 11:37:53 +0200

    change: bump treesitter version and fix config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -6,50 +6,47 @@ return {
     dependencies = {
       "nvim-treesitter/nvim-treesitter-textobjects",
     },
-    opts = {
-      ensure_installed = { "lua", "python", "bash", "json", "yaml", "markdown", "go", "gomod", "gowork", "gosum" },
-      auto_install = true,
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false,
-      },
-      indent = { enable = true },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "<C-space>",
-          node_incremental = "<C-space>",
-          scope_incremental = false,
-          node_decremental = "<bs>",
-        },
-      },
-      textobjects = {
-        select = {
-          enable = true,
-          lookahead = true,
-          keymaps = {
-            ["af"] = "@function.outer",
-            ["if"] = "@function.inner",
-            ["ac"] = "@class.outer",
-            ["ic"] = "@class.inner",
-          },
-        },
-        move = {
-          enable = true,
-          set_jumps = true,
-          goto_next_start = {
-            ["]f"] = "@function.outer",
-            ["]c"] = "@class.outer",
-          },
-          goto_previous_start = {
-            ["[f"] = "@function.outer",
-            ["[c"] = "@class.outer",
-          },
-        },
-      },
-    },
-    config = function(_, opts)
-      require("nvim-treesitter.configs").setup(opts)
+    config = function()
+      -- Ensure parsers are installed (async, runs in background)
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "LazyDone",
+        once = true,
+        callback = function()
+          require("nvim-treesitter").install({ "lua", "python", "bash", "json", "yaml", "markdown", "go", "gomod", "gowork", "gosum" })
+        end,
+      })
+
+      -- Enable treesitter highlighting and indentation for supported filetypes
+      vim.api.nvim_create_autocmd("FileType", {
+        callback = function(ev)
+          if pcall(vim.treesitter.start, ev.buf) then
+            vim.bo[ev.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+          end
+        end,
+      })
+
+      -- Textobjects config
+      require("nvim-treesitter-textobjects").setup({
+        select = { lookahead = true },
+        move = { set_jumps = true },
+      })
+
+      local ts_select = require("nvim-treesitter-textobjects.select")
+      local ts_move = require("nvim-treesitter-textobjects.move")
+
+      -- Select textobjects
+      for _, mode in ipairs({ "x", "o" }) do
+        vim.keymap.set(mode, "af", function() ts_select.select_textobject("@function.outer") end)
+        vim.keymap.set(mode, "if", function() ts_select.select_textobject("@function.inner") end)
+        vim.keymap.set(mode, "ac", function() ts_select.select_textobject("@class.outer") end)
+        vim.keymap.set(mode, "ic", function() ts_select.select_textobject("@class.inner") end)
+      end
+
+      -- Move to next/previous function/class
+      vim.keymap.set({ "n", "x", "o" }, "]f", function() ts_move.goto_next_start("@function.outer") end)
+      vim.keymap.set({ "n", "x", "o" }, "]c", function() ts_move.goto_next_start("@class.outer") end)
+      vim.keymap.set({ "n", "x", "o" }, "[f", function() ts_move.goto_previous_start("@function.outer") end)
+      vim.keymap.set({ "n", "x", "o" }, "[c", function() ts_move.goto_previous_start("@class.outer") end)
     end,
   },
 }
